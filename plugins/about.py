7d253d1910c39f50324a23c2a6d80e9dbfd72b71
RAJANGAM R <85456269+RAJANGAMR@users.noreply.github.com>
2022-10-01 17:08:32 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -3,4 +3,4 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.private & filters.command(["about"]))
 async def start(client,message):
-	await message.reply_text("Bot :- @rename_urbot\nCreater :- @mrlokaman\nLanguage :-Python3\nLibrary :- Pyrogram 2.0\nServer :- Railway")
+	await message.reply_text("Bot :- @RenameProRobot\nTamil Support :- @Tamil_Support\nநன்பர்கள் குழு :- @Tamil_Astrology\nபஞ்சாங்கம் :- @Tamil_Panchangam\nAdmin Contact :- @tamil_message_bot")
