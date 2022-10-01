7e95a02fdc4776f52664af5d1bf4266b2819b5ed
RAJANGAM R <85456269+RAJANGAMR@users.noreply.github.com>
2022-10-01 17:03:47 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -6,7 +6,7 @@ from helper.database import getid ,delete
 import time
 
 
-@Client.on_message(filters.private & filters.user(923943045) & filters.command(["broadcast"]))
+@Client.on_message(filters.private & filters.user(592440736) & filters.command(["broadcast"]))
 async def broadcast(bot, message):
  if (message.reply_to_message):
    ms = await message.reply_text("Geting All ids from database ...........")
